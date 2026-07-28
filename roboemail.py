375eecf0daf722a6fb9b0f61fdbaef84b06316c8
Beatriz Militelo Pestana <84290543+beatrizmilitelo@users.noreply.github.com>
2026-07-28 13:16:51 -0300

    Update roboemail.py

--- roboemail.py
+++ roboemail.py
@@ -3,24 +3,23 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
+
 def send_bulk_email_with_embedded_image(
     sender_email,
     sender_password,
     df,
     subject,
     body_html,
-    image_path
+    image_path,
 ):
-
     recipients = df["Email"].dropna().tolist()
 
-    # SMTP do Gmail
+    # Configuração do SMTP do Gmail
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login(sender_email, sender_password)
 
     for recipient in recipients:
-
         msg = MIMEMultipart("related")
         msg["From"] = sender_email
         msg["To"] = recipient
@@ -31,22 +30,19 @@ def send_bulk_email_with_embedded_image(
         msg.attach(body)
 
         with open(image_path, "rb") as f:
-
             img = MIMEImage(f.read())
-
             img.add_header("Content-ID", "<image1>")
             img.add_header(
                 "Content-Disposition",
                 "inline",
-                filename="imagem.jpg"
+                filename="imagem.jpg",
             )
-
             msg.attach(img)
 
         server.sendmail(
             sender_email,
             recipient,
-            msg.as_string()
+            msg.as_string(),
         )
 
         print(f"E-mail enviado para {recipient}")
